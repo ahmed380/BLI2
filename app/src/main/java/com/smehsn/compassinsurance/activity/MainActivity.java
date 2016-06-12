e07a5bf7a4b710b15bef0029a84eb15ecae50f63
Samvel Abrahamyan <sam@Samvels-MBP.Dlink>
2016-06-12 14:11:39 +0400

    Added camera permission

--- app/src/main/java/com/smehsn/compassinsurance/activity/MainActivity.java
+++ app/src/main/java/com/smehsn/compassinsurance/activity/MainActivity.java
@@ -1,51 +1,37 @@
 package com.smehsn.compassinsurance.activity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Toast;
 
 import com.smehsn.compassinsurance.R;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String EMAIL_LIST_PREFERENCES = "email_list";
 
     private static final int REQUEST_PERMISSION_CODE = 123;
-    private SharedPreferences emailListPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        initAppMode();
-        initPreferenceSources();
-
-        if(checkPermissions())
+        if(checkPermissions()) {
             openFormActivity();
-    }
-
-    private void initPreferenceSources() {
-        emailListPreferences = getSharedPreferences(EMAIL_LIST_PREFERENCES, Context.MODE_PRIVATE);
-
+            finish();
+        }
     }
 
 
-    private void initAppMode(){
-
-    }
 
     private void openFormActivity(){
         Intent intent = new Intent(this, CompleteFormActivity.class);
